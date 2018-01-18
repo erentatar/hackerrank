93930d1c4e50cbdb3842c2e159158c7745d8aae1
Eren TATAR <eren.tatar@iskur.gov.tr>
2018-01-18 11:14:56 +0300

    Algorithms > Implementation > Apple and Orange added

---
+++ HackerRank/AppleAndOrange/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace AppleAndOrange
+{
+    class Program
+    {
+        static int[] appleAndOrange(int s, int t, int a, int b, int[] apple, int[] orange)
+        {
+            var countA = apple.Select(x => a + x).Where(x => x >= s && x <= t).Count();
+            var countO = orange.Select(x => b + x).Where(x => x >= s && x <= t).Count();
+            return new int[] { countA, countO };
+        }
+
+        static void Main(String[] args)
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            int t = Convert.ToInt32(tokens_s[1]);
+            string[] tokens_a = Console.ReadLine().Split(' ');
+            int a = Convert.ToInt32(tokens_a[0]);
+            int b = Convert.ToInt32(tokens_a[1]);
+            string[] tokens_m = Console.ReadLine().Split(' ');
+            int m = Convert.ToInt32(tokens_m[0]);
+            int n = Convert.ToInt32(tokens_m[1]);
+            string[] apple_temp = Console.ReadLine().Split(' ');
+            int[] apple = Array.ConvertAll(apple_temp, Int32.Parse);
+            string[] orange_temp = Console.ReadLine().Split(' ');
+            int[] orange = Array.ConvertAll(orange_temp, Int32.Parse);
+            int[] result = appleAndOrange(s, t, a, b, apple, orange);
+            Console.WriteLine(String.Join("\n", result));
+        }
+    }
+}
