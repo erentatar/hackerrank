016aa32a95cb50e2fa6f999d9d78d30875b1d654
Eren TATAR <erentatar@hotmail.com>
2018-01-19 14:31:46 +0300

    completed

---
+++ HackerRank/SockMerchant/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace SockMerchant
+{
+    class Program
+    {
+        static int sockMerchant(int n, int[] ar)
+        {
+            var pairs = from a in ar
+                        group a by a into g
+                        where g.Count() > 1
+                        select g.Count();
+
+            return pairs.Sum(p => p / 2);
+            
+        }
+
+        static void Main(String[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] ar_temp = Console.ReadLine().Split(' ');
+            int[] ar = Array.ConvertAll(ar_temp, Int32.Parse);
+            int result = sockMerchant(n, ar);
+            Console.WriteLine(result);
+        }
+    }
+}
