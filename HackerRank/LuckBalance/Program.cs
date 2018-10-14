0b276dbd827835152ad203a85c41571cd1940cce
Eren TATAR <erentatar@hotmail.com>
2018-10-15 00:05:35 +0300

    new challenges made

---
+++ HackerRank/LuckBalance/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckBalance
+{
+    class Program
+    {
+        // Complete the luckBalance function below.
+        static int luckBalance(int k, int[][] contests)
+        {
+            int result = 0;
+            var importantLucks = new SortedList<int, int>();
+            for (int i = 0; i < contests.Length; i++)
+            {
+                if (contests[i][1] == 1)
+                    importantLucks.Add(i, contests[i][0]);
+                else if (contests[i][1] == 0)
+                    result += contests[i][0];
+            }
+
+            var sortedImportantLucks = importantLucks.OrderByDescending(v => v.Value);
+
+            foreach(var luck in sortedImportantLucks)
+            {
+                if (k > 0)
+                {
+                    result += luck.Value;
+                    k--;
+                }
+                else
+                {
+                    result -= luck.Value;
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nk = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nk[0]);
+
+            int k = Convert.ToInt32(nk[1]);
+
+            int[][] contests = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                contests[i] = Array.ConvertAll(Console.ReadLine().Split(' '), contestsTemp => Convert.ToInt32(contestsTemp));
+            }
+
+            int result = luckBalance(k, contests);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HackerRank/ZombieClusters/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZombieClusters
+{
+    class Program
+    {
+        class Result
+        {
+            public static int zombieCluster(List<string> zombies)
+            {
+                return 0;
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int zombiesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> zombies = new List<string>();
+
+            for (int i = 0; i < zombiesCount; i++)
+            {
+                string zombiesItem = Console.ReadLine();
+                zombies.Add(zombiesItem);
+            }
+
+            int result = Result.zombieCluster(zombies);
+
+            Console.WriteLine(result);
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+}
